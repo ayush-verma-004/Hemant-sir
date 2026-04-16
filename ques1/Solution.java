18774c19e386b1dd044305057885576b5959f06e
Ayush verma <139624842+ayush-verma-004@users.noreply.github.com>
2026-04-16 11:12:21 +0530

    Add solution and README for Problem 1

--- ques1/Solution.java
+++ ques1/Solution.java
@@ -1,14 +1,6 @@
+// Solution for Array Problem 1
 public class Solution {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-        System.out.println("Sum: " + sumArray(array));
-    }
-
-    public static int sumArray(int[] array) {
-        int sum = 0;
-        for (int num : array) {
-            sum += num;
-        }
-        return sum;
+        // Your solution code here
     }
 }
